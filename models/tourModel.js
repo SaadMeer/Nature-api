a3e709f929db139bab12cf4031a34293c260e257
Saad Meer <saad.wicks@gmail.com>
2022-09-01 23:26:09 +0500

    fourth commit

---
+++ models/tourModel.js
@@ -0,0 +1,59 @@
+
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulity']
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    required: 0
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    requried: [true, 'A tour must have a description']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    requried: [true, 'A tour must have a Image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
